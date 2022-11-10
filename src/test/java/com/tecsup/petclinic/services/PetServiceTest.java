868f868082224901ce90f243cfa385cee923aa11
argenis <argenis.salazar.sihuay@gmail.com>
2022-11-10 15:46:24 -0500

    Argenis lab11 commit

--- src/test/java/com/tecsup/petclinic/services/PetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/PetServiceTest.java
@@ -23,7 +23,7 @@ public class PetServiceTest {
 
 	@Autowired
 	private PetService petService;
-	
+
 	/**
 	 * 
 	 */
@@ -44,7 +44,7 @@ public class PetServiceTest {
 		assertThat(pet.getName(), is(NAME));
 
 	}
-	
+
 	/**
 	 * 
 	 */
@@ -101,7 +101,7 @@ public class PetServiceTest {
 		int OWNER_ID = 1;
 		int TYPE_ID = 1;
 
-		Pet pet = new Pet(PET_NAME, 1, 1);
+		Pet pet = new Pet(PET_NAME, 1, 1, null);
 		
 		Pet petCreated = petService.create(pet);
 		
@@ -131,7 +131,7 @@ public class PetServiceTest {
 		int UP_OWNER_ID = 2;
 		int UP_TYPE_ID = 2;
 
-		Pet pet = new Pet(PET_NAME, OWNER_ID, TYPE_ID);
+		Pet pet = new Pet(PET_NAME, OWNER_ID, TYPE_ID, null);
 
 		// Create record
 		logger.info(">" + pet);
@@ -167,21 +167,20 @@ public class PetServiceTest {
 		int OWNER_ID = 1;
 		int TYPE_ID = 1;
 
-		Pet pet = new Pet(PET_NAME, OWNER_ID, TYPE_ID);
+		Pet pet = new Pet(PET_NAME, OWNER_ID, TYPE_ID, null);
 		pet = petService.create(pet);
 		logger.info("" + pet);
 
 		try {
 			petService.delete(pet.getId());
 		} catch (PetNotFoundException e) {
-			assertThat(e.getMessage(), false);
+			fail(e.getMessage());
 		}
 			
 		try {
 			petService.findById(pet.getId());
-			assertThat(true, is(false));
+			fail("Pet id = " + pet.getId() + " has not delete");
 		} catch (PetNotFoundException e) {
-			assertThat(true, is(true));
 		} 				
 
 	}
